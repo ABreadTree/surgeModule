c68a6ba1cd5ecf114ea09ac9e1387a09e18eabcf
ABreadTree <62497798+ABreadTree@users.noreply.github.com>
2023-04-27 10:19:41 +0800

    Update ChangeRule.js

--- ChangeRule.js
+++ ChangeRule.js
@@ -27,8 +27,8 @@
     silence: false, // 是否静默运行，默认false
     cellular: "RULE", // 蜂窝数据下的模式，RULE代表规则模式，PROXY代表全局代理，DIRECT代表全局直连
     wifi: "RULE", // wifi下默认的模式
-    all_direct: ["StandByOolong", "StandByOolong_5G"], // 指定全局直连的wifi名字
-    all_proxy: [], // 指定全局代理的wifi名字
+    all_direct: [], // 指定全局直连的wifi名字
+    all_proxy: ["StandByOolong", "StandByOolong_5G"], // 指定全局代理的wifi名字
   };
   
   // load user prefs from box
